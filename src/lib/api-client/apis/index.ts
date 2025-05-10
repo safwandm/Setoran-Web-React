a6075151cd946283a91c624e2d178dab1216f4ff
haidar <haidarah134@gmail.com>
2025-05-10 19:26:52 +0700

    cleanup

--- src/lib/api-client/apis/index.ts
+++
@@ -1,9 +0,0 @@
-/* tslint:disable */
-/* eslint-disable */
-export * from './AuthApi';
-export * from './MotorApi';
-export * from './NotifikasiApi';
-export * from './PelangganApi';
-export * from './SetoranAPINETApi';
-export * from './StorageApi';
-export * from './VoucherApi';
