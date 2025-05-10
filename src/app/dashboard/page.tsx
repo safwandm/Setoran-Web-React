ef5601b87b1905270c56d4fda680fac8f2ea00c3
MuhammadGalangPradwita <mgalangpradwita16@gmail.com>
2025-05-10 18:37:11 +0700

    Menerapkan fitur sidebar

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,7 +2,8 @@ import { AppSidebar } from "@/components/app-sidebar"
 import { ChartAreaDashboard } from "./chart-area-dashboard"
 // import { DataTableDashboard } from "./data-table-dashboard"
 import { SectionCardsDasboard } from "./section-cards-dashboard"
-import { SiteHeader } from "@/components/site-header"
+// import { SiteHeader } from "@/components/site-header"
+import { IconBell } from "@tabler/icons-react"
 import {
   SidebarInset,
   SidebarProvider,
@@ -10,6 +11,10 @@ import {
 
 import data from "./data.json"
 import { DataTableDashboard } from "./data-table-dashboard"
+
+import { Button } from "@/components/ui/button"
+import { Separator } from "@/components/ui/separator"
+import { SidebarTrigger } from "@/components/ui/sidebar"
 
 export default function Page() {
   return (
@@ -23,7 +28,28 @@ export default function Page() {
     >
       <AppSidebar variant="inset" />
       <SidebarInset>
-        <SiteHeader />
+        <header className="flex h-(--header-height) shrink-0 items-center gap-2 border-b transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-(--header-height)">
+          <div className="flex w-full items-center gap-1 px-4 lg:gap-2 lg:px-6">
+            <SidebarTrigger className="-ml-1" />
+            <Separator
+              orientation="vertical"
+              className="mx-2 data-[orientation=vertical]:h-4"
+            />
+            <h1 className="text-base font-medium">Dashboard</h1>
+            <div className="ml-auto flex items-center gap-2">
+              <Button variant="ghost" asChild size="lg" className="hidden sm:flex">
+                <a
+                  href="https://github.com/shadcn-ui/ui/tree/main/apps/v4/app/(examples)/dashboard"
+                  rel="noopener noreferrer"
+                  target="_blank"
+                  className="dark:text-foreground"
+                >
+                  <IconBell className= "w-10 h-10" />
+                </a>
+              </Button>
+            </div>
+          </div>
+        </header>
         <div className="flex flex-1 flex-col">
           <div className="@container/main flex flex-1 flex-col gap-2">
             <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
