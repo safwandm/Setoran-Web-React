9456240471f69eef00696f514021bb6b4ae0834e
haidar <haidarah134@gmail.com>
2025-05-16 13:42:52 +0700

    api call voucher

--- src/app/vouchers/page.tsx
+++ src/app/vouchers/page.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import { AppSidebar } from "@/components/app-sidebar"
 import { ChartAreaInteractive } from "@/components/chart-area-interactive"
 import { SiteHeader } from "@/components/site-header"
@@ -6,10 +8,24 @@ import {
   SidebarProvider,
 } from "@/components/ui/sidebar"
 
-import data from "./data.json"
 import { DataTableVoucher } from "./data-table"
+import { useEffect, useState } from "react"
+import ApiService from "@/lib/api-client/wrapper"
+import { Voucher } from "@/lib/api-client"
 
 export default function Page() {
+
+  const [voucher, setVoucher] = useState<Voucher[]>([])
+
+  let apiService = ApiService.getInstance()
+
+  useEffect(() => {
+    apiService.voucherApi.voucherFilteredGet().then(res => {
+      setVoucher(res)
+      console.log(res)
+    })
+  }, [])
+  
   return (
     <SidebarProvider
       style={
@@ -26,7 +42,7 @@ export default function Page() {
           <div className="@container/main flex flex-1 flex-col gap-2">
             <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
              
-              <DataTableVoucher data={data} />
+              <DataTableVoucher data={voucher} />
             </div>
           </div>
         </div>
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,18 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+
+export function formatDateToLongDate(date: Date) {
+  if (!date)
+    return "";
+  
+  const day = date.getUTCDate();
+  const month = date.toLocaleString('en-GB', {
+  month: 'long',
+  timeZone: 'UTC'
+  });
+  const year = date.getUTCFullYear();
+
+  return `${day} ${month} ${year}`;
+}
