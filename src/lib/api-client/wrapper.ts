362658192ac8f418d73e86cafd1b3ee20c06bb04
haidar <haidarah134@gmail.com>
2025-05-16 09:08:08 +0700

    update api service

--- src/lib/api-client/wrapper.ts
+++ src/lib/api-client/wrapper.ts
@@ -1,7 +1,7 @@
 import dotenv from 'dotenv';
 
 import { Configuration } from './runtime';
-import { MotorApi } from './apis';
+import { AuthApi, MitraApi, MotorApi, NotifikasiApi, PelangganApi, PenggunaApi, SetoranAPINETApi } from './apis';
 import { VoucherApi } from './apis';
 // register api lain kalau butuh
 
@@ -12,8 +12,14 @@ class ApiService {
   private static instance: ApiService;
   private config: Configuration
 
+  public authApi!: AuthApi;
+  public pelangganApi!: PelangganApi;
+  public penggunaApi!: PenggunaApi;
+  public mitraApi!: MitraApi;
   public motorApi!: MotorApi;
   public voucherApi!: VoucherApi;
+  public setoranApi!: SetoranAPINETApi;
+  public notifikasiApi!: NotifikasiApi;
 
   private constructor(config?: Configuration) {
     if (config == undefined)
@@ -27,8 +33,14 @@ class ApiService {
   }
 
   private createApis() {
+    this.authApi = new AuthApi(this.config);
+    this.pelangganApi = new PelangganApi(this.config);
+    this.penggunaApi = new PenggunaApi(this.config);
+    this.mitraApi = new MitraApi(this.config);
     this.motorApi = new MotorApi(this.config);
     this.voucherApi = new VoucherApi(this.config);
+    this.setoranApi = new SetoranAPINETApi(this.config);
+    this.notifikasiApi = new NotifikasiApi(this.config);
   }
 
   public static getInstance(): ApiService {
