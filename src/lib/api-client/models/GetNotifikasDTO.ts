76b821e293c18abef2757b5979dd6f0f648f8656
haidar <haidarah134@gmail.com>
2025-05-24 09:37:25 +0700

    update api lib again

--- src/lib/api-client/models/GetNotifikasDTO.ts
+++ src/lib/api-client/models/GetNotifikasDTO.ts
@@ -69,6 +69,12 @@ export interface GetNotifikasDTO {
      * @memberof GetNotifikasDTO
      */
     isRead?: boolean;
+    /**
+     * 
+     * @type {Date}
+     * @memberof GetNotifikasDTO
+     */
+    waktuNotifikasi?: Date;
 }
 
 
@@ -97,6 +103,7 @@ export function GetNotifikasDTOFromJSONTyped(json: any, ignoreDiscriminator: boo
         'navigasi': json['navigasi'] == null ? undefined : TargetNavigasiFromJSON(json['navigasi']),
         'dataNavigasi': json['dataNavigasi'] == null ? undefined : json['dataNavigasi'],
         'isRead': json['isRead'] == null ? undefined : json['isRead'],
+        'waktuNotifikasi': json['waktuNotifikasi'] == null ? undefined : (new Date(json['waktuNotifikasi'])),
     };
 }
 
@@ -118,6 +125,7 @@ export function GetNotifikasDTOToJSONTyped(value?: GetNotifikasDTO | null, ignor
         'navigasi': TargetNavigasiToJSON(value['navigasi']),
         'dataNavigasi': value['dataNavigasi'],
         'isRead': value['isRead'],
+        'waktuNotifikasi': value['waktuNotifikasi'] == null ? undefined : ((value['waktuNotifikasi']).toISOString()),
     };
 }
 
